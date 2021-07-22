0671204ffd2c7133ff08aaa1c96ad64a5c6d703a
Daniel Hong <dan8492@gmail.com>
2021-07-22 14:51:19 -0700

    added functionality to the callback function so that my car turns accordingly

--- main.js
+++ main.js
@@ -3,5 +3,13 @@ var $car = document.querySelector('.car');
 document.addEventListener('keydown', turn);
 
 function turn(event) {
-
+  if (event.key === 'ArrowRight') {
+    $car.className = 'face-east';
+  } else if (event.key === 'ArrowLeft') {
+    $car.className = 'face-west';
+  } else if (event.key === 'ArrowUp') {
+    $car.className = 'face-north';
+  } else if (event.key === 'ArrowDown') {
+    $car.className = 'face-south';
+  }
 }
